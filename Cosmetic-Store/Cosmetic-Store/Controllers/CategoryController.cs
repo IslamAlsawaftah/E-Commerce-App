3d6a99455bba2e6df35cdeba49e41b31cba0991e
Ola M AL-Shlool <olashlool94@gmail.com>
2022-05-31 03:14:43 +0300

    Sprint-01-Milestone-01

---
+++ Cosmetic-Store/Cosmetic-Store/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Cosmetic_Store.Models;
+using Cosmetic_Store.Models.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cosmetic_Store.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ICategory _category;
+        public CategoryController(ICategory category)
+        {
+            _category = category;
+        }
+        public async Task<IActionResult> Index()
+        {
+            //List<Category> categoriesList = new List<Category>()
+            //{
+            //    new Category() { CategoryId = 1, Name = "Mascara" },
+            //    new Category() { CategoryId = 2, Name = "Lipstick" },
+            //    new Category() { CategoryId = 3, Name = "Powder" }
+            //};
+
+            var listOfCategories = await _category.GetCategories();
+            return View(listOfCategories);
+        }
+        public async Task<IActionResult> GategoryDetail(int id)
+        {
+            var category = await _category.GetCategoryById(id);
+            return View(category);
+        }
+    }
+}
---
+++ Cosmetic-Store/Cosmetic-Store/Models/Interface/ICategory.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cosmetic_Store.Models.Interface
+{
+    public interface ICategory
+    {
+        public Task<Category> Create (Category category);
+        public Task<Category> GetCategoryById(int id);
+        public Task<List<Category>> GetCategories();
+        public Task<Category> Update (int id , Category category);
+        public Task Delete (int id);
+        public Task AddProduct(int categoryId, int croductId);
+        public Task RemoveProduct(int categoryId, int productId);
+    }
+}
